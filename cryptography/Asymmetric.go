df8d281f61f06ccf1368b89a7cc9660f2388cde9
canlot <jakobsasmuzki@gmx.de>
2024-01-28 19:17:33 +0100

    improving

--- cryptography/Asymmetric.go
+++ cryptography/Asymmetric.go
@@ -1,4 +1,4 @@
-package internals
+package cryptography
 
 func CreateRSAKeys() {
 
--- internals/Store.go
+++ internals/Store.go
@@ -2,6 +2,9 @@ package internals
 
 import (
 	"errors"
+	"github.com/boltdb/bolt"
+	"golang.org/x/crypto/scrypt"
+	"time"
 )
 
 type DBStore struct {
@@ -9,18 +12,55 @@ type DBStore struct {
 
 var Store DBStore
 
-func (s DBStore) DatabaseEmpty() (bool, error) {
-	db, err := Database.Begin(false)
+func (s DBStore) IsDatabaseEmpty() (bool, error) {
+	var err error
+	tx, err := Database.Begin(false)
+	tx.Commit()
 	if err != nil {
 		return false, err
 	}
-	bucket := db.Bucket([]byte(BucketLoginInformation))
+	var empty bool
+	empty, err = IsBucketEmpty(tx, BucketLoginInformation)
+	if err != nil {
+		return false, err
+	}
+	if !empty {
+		return false, nil
+	}
+	empty, err = IsBucketEmpty(tx, BucketLoginPrivateKeyEncrypted)
+	if err != nil {
+		return false, err
+	}
+	if !empty {
+		return false, nil
+	}
+	empty, err = IsBucketEmpty(tx, BucketLoginPrivateKeyEncrypted)
+	if err != nil {
+		return false, err
+	}
+	if !empty {
+		return false, nil
+	}
+
+	return true, nil
+}
+func IsBucketEmpty(tx *bolt.Tx, bucketName string) (bool, error) {
+	bucket := tx.Bucket([]byte(bucketName))
 	if bucket == nil {
-		return errors.New("Bucket not created")
+		return false, errors.New("Bucket not created: " + bucketName)
 	}
 	c := bucket.Cursor()
 	first, _ := c.First()
 	if first != nil {
-		return false
+		return false, nil
 	}
+	return true, nil
+}
+func CreateFirstLoginWithRSAKeys(username, password string) error {
+	createTime := time.Now()
+	passwordHash, err := scrypt.Key([]byte(password), nil, 32768, 8, 1, 32)
+	if err != nil {
+		return err
+	}
+	u := uuid.New()
 }
