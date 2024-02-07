659f9c99c1457ece681784e3ea06657f4f79202c
canlot <j.sasmuzki@simplicity.ag>
2024-02-07 19:12:27 +0100

    cdf

--- cmd/delete/delete.go
+++ cmd/delete/delete.go
@@ -4,23 +4,19 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package delete
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var DeleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
+	Short: "Delete database entry",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
-	},
 }
 
 func init() {
--- cmd/list/list.go
+++ cmd/list/list.go
@@ -11,12 +11,7 @@ import (
 var ListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Show an overview of available logins, data or files",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  ``,
 }
 
 func init() {
