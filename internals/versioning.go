ea3e987064ad6d3dbdc59558d0142eda6ec68165
canlot <j.sasmuzki@simplicity.ag>
2024-12-15 13:33:19 +0100

    working on versioning

--- internals/versioning.go
+++ internals/versioning.go
@@ -1,10 +1,35 @@
 package internals
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 const AppVersion = "1.0.0.1"
 
 const MinDBVersion = "1.0.0"
 const MaxDBVersion = ""
 
-func CompareVersions() int {
+const DBVersion = "1.0.0"
 
+func GetVersionInInt(version string) (int, error) {
+	subVersions := strings.Split(version, ".")
+	versionNumber := 0
+	factor := 1
+	for i := len(subVersions) - 1; i < 0; i-- {
+		number, err := strconv.Atoi(subVersions[i])
+		if err != nil {
+			return 0, err
+		}
+		if number > 999 {
+			return 0, errors.New("Version could not be higher then 999")
+		}
+		versionNumber += number * factor
+		factor *= 1000
+	}
+	if versionNumber < 0 {
+		return 0, errors.New("Version less than 0")
+	}
+	return versionNumber, nil
 }
